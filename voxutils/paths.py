d80acb3bf406977ac74b6cec25c8c4b8e7be9f40
Douglas Bagnall <douglas@paradise.net.nz>
2012-08-17 14:35:07 +1200

    Add the free dictionaries; ignore beep and unisyn for now

--- voxutils/paths.py
+++ voxutils/paths.py
@@ -2,7 +2,7 @@ from os.path import dirname, join, abspath
 
 ROOT = dirname(dirname(abspath(__file__)))
 DICT = join(ROOT, 'dict')
-
+DICT_NONFREE = join(ROOT, 'dict', 'non-free')
 
 CORPORA_DIR = join(ROOT, 'corpora')
 RESAMPLED_16K_DIR = join(CORPORA_DIR, '16k')
@@ -13,7 +13,7 @@ BASE_CORPORA = ['voxforge', 'wellington', 'hansard']
 CORPORA = {x: join(CORPORA_DIR, x) for x in BASE_CORPORA}
 
 CMUDICT = join(DICT, 'cmudict.0.7a')
-UNISYN_DICT = join(DICT, 'unisyn-nz.txt')
+UNISYN_DICT = join(DICT_NONFREE, 'unisyn-nz.txt')
 VOXFORGE_DICT = join(DICT, 'VoxForgeDict')
-BEEP_DICT = join(DICT, 'beep', 'beep-1.0')
+BEEP_DICT = join(DICT_NONFREE, 'beep', 'beep-1.0')
 ESPEAK_DICT = join(DICT, 'espeak-corpus+50k.txt')
